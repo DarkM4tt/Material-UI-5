c657532865fdf7503fcdecde2a856449048a1846
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-12-07 00:51:08 +0530

    Done CheckBoxes.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import "./App.css";
 // import MuiButton from "./components/MuiButton";
 // import MuiTextField from "./components/MuiTextField";
 // import MuiSelect from "./components/MuiSelect";
-import MuiRadioButton from "./components/MuiRadioButton";
+// import MuiRadioButton from "./components/MuiRadioButton";
+import MuiCheckBox from "./components/MuiCheckBox";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       {/* <MuiButton /> */}
       {/* <MuiTextField /> */}
       {/* <MuiSelect /> */}
-      <MuiRadioButton />
+      {/* <MuiRadioButton /> */}
+      <MuiCheckBox />
     </div>
   );
 }
