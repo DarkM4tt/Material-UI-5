4a3c4c137efbbf4fd74c2762e66dad5f1f755c42
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-12-06 21:17:16 +0530

    Done Radio Buttons.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import "./App.css";
 //import MuiTypography from "./components/MuiTypography";
 // import MuiButton from "./components/MuiButton";
-import MuiTextField from "./components/MuiTextField";
-import MuiSelect from "./components/MuiSelect";
+// import MuiTextField from "./components/MuiTextField";
+// import MuiSelect from "./components/MuiSelect";
+import MuiRadioButton from "./components/MuiRadioButton";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       {/* <MuiTypography /> */}
       {/* <MuiButton /> */}
       {/* <MuiTextField /> */}
-      <MuiSelect />
+      {/* <MuiSelect /> */}
+      <MuiRadioButton />
     </div>
   );
 }
