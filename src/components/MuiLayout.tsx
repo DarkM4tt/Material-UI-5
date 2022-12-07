efe0d32eadfd2f9edfe9b4a9f667f395548c71ed
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-12-07 17:03:08 +0530

    Stack & Grid.

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,30 +1,52 @@
-import { Box } from "@mui/material";
+import { Box, Stack, Divider, Grid } from "@mui/material";
 
 const MuiLayout = () => {
   return (
     <>
-      <Box
-        sx={{
-          backgroundColor: "primary.main",
-          color: "white",
-          height: "100px",
-          width: "100px",
-          padding: "16px",
-          "&:hover": {
-            backgroundColor: "primary.light",
-          },
-        }}
+      <Stack
+        sx={{ border: "1px solid" }}
+        direction="row"
+        spacing={2}
+        divider={<Divider orientation="vertical" flexItem />}
       >
-        Codevolution
-      </Box>
+        <Box
+          sx={{
+            backgroundColor: "primary.main",
+            color: "white",
+            height: "100px",
+            width: "100px",
+            padding: "16px",
+            "&:hover": {
+              backgroundColor: "primary.light",
+            },
+          }}
+        >
+          Codevolution
+        </Box>
 
-      <Box
-        display="flex"
-        height="100px"
-        width="100px"
-        bgcolor="success.light"
-        p={2}
-      ></Box>
+        <Box
+          display="flex"
+          height="100px"
+          width="100px"
+          bgcolor="success.light"
+          p={2}
+        ></Box>
+      </Stack>
+
+      <Grid container my={4}>
+        <Grid item xs={3}>
+          <Box bgcolor="primary.light">Item 1</Box>
+        </Grid>
+        <Grid item xs={9}>
+          <Box bgcolor="secondary.dark">Item 2</Box>
+        </Grid>
+        <Grid item xs={4}>
+          <Box bgcolor="primary.dark">Item 3</Box>
+        </Grid>
+        <Grid item xs={8}>
+          <Box bgcolor="secondary.light">Item 4</Box>
+        </Grid>
+      </Grid>
     </>
   );
 };
